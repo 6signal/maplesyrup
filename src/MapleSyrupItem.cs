a46aaef9bf5c0dddb3945e7427987555de1cea35
Omega <Omega98@users.noreply.github.com>
2020-05-22 17:50:09 -0400

    Added maple syrup item
    Added maple syrup item based on resin item
    Texture is a hue/saturation modified version of the resin texture

---
+++ src/MapleSyrupItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+
+namespace VSExampleMods
+{
+    public class MapleSyrupItem : Item
+    {
+    }
+}
