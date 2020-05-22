c412f7f1043941788275371b273f2a53abbcb166
Omega <Omega98@users.noreply.github.com>
2020-05-22 00:02:08 -0400

    Add trampoline block
    Add trampoline block example

---
+++ src/MapleSyrupMod.cs
@@ -0,0 +1,16 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+
+namespace VSExampleMods
+{
+    public class MapleSyrupMod : ModSystem
+    {
+
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            api.RegisterBlockClass("trampoline", typeof(TrampolineBlock));
+        }
+    }
+}
